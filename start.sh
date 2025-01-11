ba6e8135b20c4c1d681309a425d42c27571543a6
Powerbute <64857329+powerbute@users.noreply.github.com>
2025-01-11 13:11:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ lsb_release -a
 cat /etc/*-release
 
 # Установка Java 21
-rpm -i java-21-openjdk-21.0.5.0.11-1.fc41.x86_64.rpm
+dnf install java-17-openjdk
 
 # Запуск JAR-файла
 java -jar paper-1.21.4-85.jar
