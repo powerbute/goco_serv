06d96063c314a49cfbde6c0d6e5324d827c99902
Powerbute <64857329+powerbute@users.noreply.github.com>
2025-01-11 13:10:08 +0300

    1

--- start.sh
+++ start.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-# Получаем информацию об архитектуре из файла /proc/cpuinfo
-arch=$(grep "CPU architecture" /proc/cpuinfo | awk '{print $3}')
-
-# Выводим результат
-echo "Архитектура сервера: $arch"
-
 lsb_release -a
 cat /etc/*-release
 
 # Установка Java 21
-dnf install java-21-openjdk
-
-# Проверка успешной установки
-if [ $? -eq 0 ]; then
-  echo "Java 21 установлена успешно"
-else
-  echo "Ошибка при установке Java 21"
-  exit 1
-fi
+rpm -i java-21-openjdk-21.0.5.0.11-1.fc41.x86_64.rpm
 
 # Запуск JAR-файла
 java -jar paper-1.21.4-85.jar
