538711f45730ec66528f33c5014db6fdc4ce380f
Powerbute <64857329+powerbute@users.noreply.github.com>
2025-01-11 12:55:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Получаем информацию о дистрибутиве из различных файлов
-lsb_release -a
-cat /etc/*-release
+# Установка Java 21
+sudo dnf install java-21-openjdk-devel
 
-# Получаем более подробную информацию о системе
-uname -a
+# Проверка успешной установки
+if [ $? -eq 0 ]; then
+  echo "Java 21 установлена успешно"
+else
+  echo "Ошибка при установке Java 21"
+  exit 1
+fi
+
+# Запуск JAR-файла
+java -jar paper-1.21.4-85.jar
